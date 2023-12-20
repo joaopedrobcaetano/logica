cb8eba095abf14b34a73b481c20c3160b6fa1e7e
João Pedro <joaopedrobcaetano@gmail.com>
2023-12-20 10:52:27 -0300

    aprendendo variáveis e constantes

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Olá, mundo!")
+// saída
+console.log("Digite o nome do seu jogador")
+
+// declara uma variável
+let nickname = "JP"
+
+// concatenando uma mensagem fixa + uma variável
+console.log("Bem vindo " + nickname)
+console.log(nickname + " entrou no servidor")
+
+// criando constantes
+const notificacao = "Pokemon Go diz: "
+
+console.log(notificacao + "tem um novo Pokemon na região")
