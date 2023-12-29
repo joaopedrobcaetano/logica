a9beb5762f2f458de06c8499dfd673a600ec407a
João Pedro <joaopedrobcaetano@gmail.com>
2023-12-29 17:43:42 -0300

    função main

--- funcoes.js
+++ funcoes.js
@@ -5,7 +5,29 @@ function torrar() {
     injetarPao()
 }
 
-function injetarPao(){
+function injetarPao() {
     console.log("preparando o pão")
     console.log("finalizado")
+}
+
+// outra função
+
+main()
+
+function main() {
+    trainingTeam()
+    selectSquad()
+    goToMatch()
+}
+
+function trainingTeam() {
+    console.log("O time está treinando")
+}
+
+function selectSquad() {
+    console.log("Time escalado para o jogo")
+}
+
+function goToMatch() {
+    console.log("O time está indo para o jogo")
 }
