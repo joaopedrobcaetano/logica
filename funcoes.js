209570a5a82f051969d49f271bb6eac882291853
João Pedro <joaopedrobcaetano@gmail.com>
2023-12-29 17:24:18 -0300

    aprendendo funções

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+torrar()
+
+function torrar() {
+    console.log("torrando pão")
+    injetarPao()
+}
+
+function injetarPao(){
+    console.log("preparando o pão")
+    console.log("finalizado")
+}
