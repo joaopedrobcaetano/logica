cc6b3f363341778dd336266354dbdaecbe715e9b
João Pedro <joaopedrobcaetano@gmail.com>
2024-01-04 13:33:35 -0300

    teste json

--- teste.js
+++ teste.js
@@ -1,2 +1,11 @@
-let x = 1+1
-console.log(x)
+let invoice = {
+    name: "João Pedro",
+    age: 30,
+    products: {
+        0: ["mouse", 29.90],
+        1: ["teclado", 129.90],
+        2: ["monitor", 899.90],
+    }
+}
+
+console.log(invoice.age)
